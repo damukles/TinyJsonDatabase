b90d04618c21d1fa596e8b58f1e431f8e4c15887
Daniel Mueller <daniel.mueller@cloudathome.ch>
2018-06-01 17:58:41 +0200

    moved blob to file

---
+++ File/IFileDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TinyBlockStorage.File
+{
+    public interface IFileDatabase
+    {
+        void Insert(FileModel blob);
+        void Delete(FileModel blob);
+        void Update(FileModel blob);
+        FileModel Find(Guid id);
+        IEnumerable<FileModel> FindBy(string fileName);
+    }
+}
+
--- FileTestApp/Program.cs
+++ FileTestApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using TinyBlockStorage.Blob;
+using TinyBlockStorage.File;
 
 namespace TestApp
 {
@@ -19,9 +19,9 @@ namespace TestApp
                 var fileName = Path.GetFileName(filePath);
                 byte[] fileContent = File.ReadAllBytes(filePath);
 
-                using (var db = new BlobDatabase(dbPath))
+                using (var db = new FileDatabase(dbPath))
                 {
-                    db.Insert(new BlobModel()
+                    db.Insert(new FileModel()
                     {
                         Id = Guid.NewGuid(),
                         FileName = fileName,
@@ -30,7 +30,7 @@ namespace TestApp
                     });
                 }
 
-                using (var db = new BlobDatabase(dbPath))
+                using (var db = new FileDatabase(dbPath))
                 {
                     var entries = db.FindBy(fileName, fileLastEdited);
 
@@ -46,11 +46,19 @@ namespace TestApp
                 }
             }
 
-            using (var db = new BlobDatabase(dbPath))
+            using (var db = new FileDatabase(dbPath))
             {
-                var data = db.Find(Guid.Parse("1f90ea04-ed60-425e-810f-594f2cd6801b")).BlockData;
+                var data = db.Find(Guid.Parse("1f90ea04-ed60-425e-810f-594f2cd6801b"))?.BlockData;
 
-                File.WriteAllBytes("out.jpg", data);
+                if (data == null)
+                {
+                    Console.WriteLine("Cloud not find data by GUID.");
+                }
+                else
+                {
+                    File.WriteAllBytes("out.jpg", data);
+                    Console.WriteLine("Data written to out.jpg");
+                }
             }
 
         }
